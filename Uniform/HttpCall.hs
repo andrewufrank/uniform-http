188904ca4151fcfac1ae7bf9468a1693d1f7549c
Andrew Frank <frank@geoinfo.tuwien.ac.at>
2018-04-04 18:35:59 +0200

    removed old post (post7 post8)

--- Uniform/HttpCall.hs
+++ Uniform/HttpCall.hs
@@ -39,11 +39,11 @@ makeRequest :: URI -> ErrIO Conduit.Request
 makeRequest dest = Http.parseRequest . t2s . uriT $ dest
 
 
-callHTTP8post :: Bool -> Text -> URI -> Text -> Text -> ErrIO Text
--- post a body to the  url given as a type given
---application/sparql-update
-callHTTP8post debug appType dest path txt =
-    callHTTP9post debug appType dest path (b2bl . t2b $ txt)
+-- callHTTfiP8pFost :: Bool -> Text -> URI -> Text -> Text -> ErrIO Text
+-- -- post a body to the  url given as a type given
+-- --application/sparql-update
+-- callHTTP8post debug appType dest path txt =
+--     callHTTP9post debug appType dest path (b2bl . t2b $ txt)
 
 callHTTP9post :: Bool -> Text -> URI -> Text -> LazyByteString -> ErrIO Text
 -- post a body to the  url given as a type given
@@ -90,60 +90,59 @@ callHTTP10post debug appType dest path txt vars timeout = do
     when debug $ putIOwords [showT (Http.getResponseHeader "Content-Type" res)]
     let res2 = bb2t . bl2b . Http.getResponseBody $ res :: Text
     -- stops if not an UTF8 encoded text
---    when False $ putIOwords ["callHTTP8post response: ", res2]
     return res2
 
--- TODO merge the post7 and post9
--- post7 has a query paramter with
-makeHttpPost7 :: Bool ->  URI -> Text -> HttpVarParams -> Text -> Text ->  ErrIO Text
--- post a body to the  url given as a type given
---application/sparql-update
--- path is query .. or something which is type,value pairs
--- is not used anymore?
-makeHttpPost7 debug dest path query appType txt = do
-    callHTTP10post debug appType ( dest) path (b2bl . t2b $ txt) query (Just 300)
+-- -- TODO merge the post7 and post9
+-- -- post7 has a query paramter with
+-- makeHttpPost7 :: Bool ->  URI -> Text -> HttpVarParams -> Text -> Text ->  ErrIO Text
+-- -- post a body to the  url given as a type given
+-- --application/sparql-update
+-- -- path is query .. or something which is type,value pairs
+-- -- is not used anymore?
+-- makeHttpPost7 debug dest path query appType txt = do
+--     callHTTP10post debug appType ( dest) path (b2bl . t2b $ txt) query (Just 300)
 
 
 
 formatQuery :: (Text, Maybe Text) -> (ByteString, Maybe ByteString)
 formatQuery (a, mb) = (t2b a, fmap t2b mb)
 --
-makeHttpPost7x  :: Bool ->  URI -> Text -> HttpVarParams -> Text -> Text ->  ErrIO Text
--- post a body to the  url given as a type given
---application/sparql-update
--- path is query .. or something which is type,value pairs
-makeHttpPost7x  debug dest path vars appType txt = do
-    req1 <- makeRequest dest
-    let length = lengthChar txt
-    let req2 = Http.setRequestBodyLBS  (b2bl . t2b $ txt)
-                $ Http.setRequestHeader "Content-Type" [t2b appType]
-                $ Http.setRequestMethod "POST"
-                $ Http.setRequestPath (t2b path)
-                $ Http.setRequestQueryString
-                            (map formatQuery . unHttpVarParams $ vars)
---                $ Conduit.ResponseTimeout 300000 -- msecs
-                req1
-                    {Conduit.responseTimeout = Conduit.responseTimeoutMicro 300000000}
-----            }
-    when debug $ putIOwords ["makeHttpPost7", showT req2, "text length", showT length]
-    res <- callIO $
-        do
-                 Http.httpLBS req2
-            `catchError` \e -> do
-                     putIOwords ["makeHttpPost7  error caught 3", showT e
-                            , "\n should not occur - caught by callIO ??"
-                            , "\n note hint: replace localhost by 127.0.0.1"
-                            ,  "\n", showT req2]
-                     fail . unwords $  [ "makeHttpPost7 httperror 3", show e]
-                                         -- is in the IO monad, not ErrIO
+-- makeHttpPost7x  :: Bool ->  URI -> Text -> HttpVarParams -> Text -> Text ->  ErrIO Text
+-- -- post a body to the  url given as a type given
+-- --application/sparql-update
+-- -- path is query .. or something which is type,value pairs
+-- makeHttpPost7x  debug dest path vars appType txt = do
+--     req1 <- makeRequest dest
+--     let length = lengthChar txt
+--     let req2 = Http.setRequestBodyLBS  (b2bl . t2b $ txt)
+--                 $ Http.setRequestHeader "Content-Type" [t2b appType]
+--                 $ Http.setRequestMethod "POST"
+--                 $ Http.setRequestPath (t2b path)
+--                 $ Http.setRequestQueryString
+--                             (map formatQuery . unHttpVarParams $ vars)
+-- --                $ Conduit.ResponseTimeout 300000 -- msecs
+--                 req1
+--                     {Conduit.responseTimeout = Conduit.responseTimeoutMicro 300000000}
+-- ----            }
+--     when debug $ putIOwords ["makeHttpPost7", showT req2, "text length", showT length]
+--     res <- callIO $
+--         do
+--                  Http.httpLBS req2
+--             `catchError` \e -> do
+--                      putIOwords ["makeHttpPost7  error caught 3", showT e
+--                             , "\n should not occur - caught by callIO ??"
+--                             , "\n note hint: replace localhost by 127.0.0.1"
+--                             ,  "\n", showT req2]
+--                      fail . unwords $  [ "makeHttpPost7 httperror 3", show e]
+--                                          -- is in the IO monad, not ErrIO
 
 
-    let statusCode = Http.getResponseStatusCode res
-    when debug $ putIOwords ["makeHttpPost7 The status code was: ", showT statusCode]
-    when debug $ putIOwords ["\t", showT (Http.getResponseHeader "Content-Type" res)]
-    let res2 = bb2t . bl2b . Http.getResponseBody $ res :: Text
-    -- stops if not an UTF8 encoded text
---    when True $ putIOwords ["makeHttpPost7 response: ", res2]
-    return res2
+--     let statusCode = Http.getResponseStatusCode res
+--     when debug $ putIOwords ["makeHttpPost7 The status code was: ", showT statusCode]
+--     when debug $ putIOwords ["\t", showT (Http.getResponseHeader "Content-Type" res)]
+--     let res2 = bb2t . bl2b . Http.getResponseBody $ res :: Text
+--     -- stops if not an UTF8 encoded text
+-- --    when True $ putIOwords ["makeHttpPost7 response: ", res2]
+--     return res2
 
 
